4ca4ee595372a8ef22587d1e61224d39e21a6e68
coollee1001 <108321928+coollee1001@users.noreply.github.com>
2022-06-28 15:04:54 +0900

    Update 3.RadiansToDegrees.py
    日本語追記

--- 20220628/3.RadiansToDegrees.py
+++ 20220628/3.RadiansToDegrees.py
@@ -1,7 +1,8 @@
 #라디안을 각도로 바꾸기
+#ラジアンを角度に変える
 from math import degrees, pi
 
-# 방법1
+# 방법1　方法１
 
 def RadiansToDegrees(rad):
     return degrees(rad)
@@ -9,7 +10,7 @@ def RadiansToDegrees(rad):
 print(RadiansToDegrees(0.22)) #12.60507149287811
 print(RadiansToDegrees(1.28)) #73.33859777674537
 
-# 방법2
+# 방법2　方法２
 
 def RadiansToDegrees_2(rad):
     return (rad*180.0)/pi
